44257a4ac0a7d2a6b361fc484dc348f535d4a28b
Taka-bnbn <mizutaka2005@gmail.com>
2024-11-27 13:19:50 +0900

    test

---
+++ test.bash
@@ -0,0 +1,27 @@
+#!/bin/bash -xv
+# SPDX-FileCopyrightText :2024 Takaya Mizumaki
+# SPDX-License-Identifier: BSD-3=Clause
+
+
+ng (){
+	echo ${1}行目が違うぞボケ
+	res=1
+}
+
+res=0
+
+###NORMAL INPUT###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+###STRANGE INPUT###
+out=$(echo あ| ./plus)
+[ "$?" = 1 ]	 || ng "$LINENO"
+[ "${out}" = "" ]|| ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]   	  || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "$res" = 0 ]  && echo OK
+exit $res
