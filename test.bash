11f397ee850eaceafe575e37d84cf097fdf8af12
Taka-bnbn <mizutaka2005@gmail.com>
2024-11-29 10:34:17 +0900

    修正済み

--- test.bash
+++ test.bash
@@ -10,15 +10,15 @@ ng () {
 res=0
 
 ### NORMAL INPUT ###
-out=$(./tedorikeisan 1000000)
+out=$(echo "1000000" | ./tedorikeisan)
 expected="年収 1,000,000 円の場合，手取りは約 794,500 円です．
 205,500 円は国の下へ去りました．"
 [ "${out}" = "${expected}" ] || ng "$LINENO"
 
 ### STRANGE INPUT ###
-out=$(./tedorikeisan 2>&1)
+out=$(echo "" | ./tedorikeisan 2>&1)
 [ "$?" -ne 0 ] || ng "$LINENO"
-expected_error="使い方: ./kadai [年収]"
+expected_error="入力エラー: 年収は正の数である必要があります。"
 [ "${out}" = "${expected_error}" ] || ng "$LINENO"
 
 ### CHECK RESULT ###
